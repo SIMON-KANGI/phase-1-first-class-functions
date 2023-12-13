25076e89a7b18f5e3605af8e2f62cd8cd1486303
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-13 23:17:59 +0300

    completed test

---
+++ index.js
@@ -0,0 +1,9 @@
+const receivesAFunction=(series)=>{
+return series()
+}
+function returnsANamedFunction(){
+return function movies(){}
+}
+function returnsAnAnonymousFunction(){
+return function(){}
+}
